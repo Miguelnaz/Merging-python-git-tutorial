1070fd364bb7d1e9212e2329ca61211624165229
Miguelnaz <nazarage@hotmail.com>
2017-03-15 16:31:19 +0100

    Added listString

---
+++ Tut1/listString.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 16:23:45 2017
+
+@author: nazar
+"""
+
+#list and strings
+
+fileList=list(range(2000,2020,2))
+
+print(fileList)
+
+print(fileList[5])
+#Pritns fom 2 to 4
+print(fileList[2:5])
+#Prints from 0 to 3
+print(fileList[:4])
+#Prints from 0 to three positions before the lastone
+print(fileList[:-3])
+
+aString="String"
+print(aString)
+print(aString[1:3])
