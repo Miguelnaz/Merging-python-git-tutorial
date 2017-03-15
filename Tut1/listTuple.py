3cd3210a1483dc033d280be71a353e222e61612c
Miguelnaz <nazarage@hotmail.com>
2017-03-15 13:13:09 +0100

    Added listTuple

---
+++ Tut1/listTuple.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 13:06:15 2017
+
+@author: nazar
+"""
+
+#list and tuple
+
+fileList=[150,560,780,9]
+print(fileList)
+
+print(fileList[0])
+
+fileList.append(890)
+
+print(fileList)
+
+tuple1=(150,560,80)
